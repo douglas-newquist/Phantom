1e069c7c6cee1591c1dd3b9c83559f4984da1e44
Douglas Newquist <doug.newquist@gmail.com>
2022-07-07 15:54:39 -0600

    Use random sprite if multiple are given

--- Assets/Scripts/Tiles/SimpleTile.cs
+++ Assets/Scripts/Tiles/SimpleTile.cs
@@ -7,5 +7,19 @@ namespace Phantom
 	public class SimpleTile : Tile
 	{
 		public Sprite[] sprites;
+
+		public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+		{
+			tileData.color = color;
+			tileData.colliderType = colliderType;
+			tileData.gameObject = gameObject;
+			tileData.flags = flags;
+			tileData.transform = transform;
+
+			if (sprites.Length > 0)
+				tileData.sprite = sprites[Random.Range(0, sprites.Length)];
+			else
+				tileData.sprite = sprite;
+		}
 	}
 }
