2933fbfac1c8f38cc85538cf42ac248e4dea0cdf
Douglas Newquist <doug.newquist@gmail.com>
2022-07-09 12:56:48 -0600

    Comments and extra attitudes

--- Assets/Scripts/Attitude.cs
+++ Assets/Scripts/Attitude.cs
@@ -1,13 +1,45 @@
 namespace Phantom
 {
+	/// <summary>
+	/// Defines the general stance of an entity towards another entity
+	/// </summary>
 	[System.Flags]
 	public enum Attitude
 	{
-		None,
+		/// <summary>
+		/// Entity has no attitude or is incapable of having one to another entity
+		/// </summary>
+		NoAttitude,
+
+		/// <summary>
+		/// Entity is neither friendly or hostile but will still defend itself
+		/// </summary>
 		Neutral = 1,
+
 		Friendly = 2,
+
 		Hostile = 4,
+
+		/// <summary>
+		/// Entity is unpredictable and will act unpredictably towards other entities
+		/// </summary>
 		Enigmatic = 8,
+
+		/// <summary>
+		/// Entity will completely ignore another
+		/// </summary>
+		Dismissive = 16,
+
+		/// <summary>
+		/// Entities that will not actively harm this one
+		/// </summary>
+		NonHostile = Neutral | Friendly | Dismissive,
+
+		/// <summary>
+		/// Entities that will not actively help this one
+		/// </summary>
+		NonFriendly = Neutral | Hostile | Enigmatic | Dismissive,
+
 		AnyAttitude = ~0
 	}
 }
