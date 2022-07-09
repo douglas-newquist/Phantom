440ee6415ada80ff3edeeb598e4360066f7a17f2
Douglas Newquist <doug.newquist@gmail.com>
2022-07-09 14:18:46 -0600

    Provide access to unity elements

--- Assets/Scripts/IEntity.cs
+++ Assets/Scripts/IEntity.cs
@@ -1,7 +1,12 @@
+using UnityEngine;
+
 namespace Phantom
 {
 	public interface IEntity
 	{
+		GameObject gameObject { get; }
+		Transform transform { get; }
+
 		bool InCombat { get; }
 		bool IsAlive { get; }
 		Attitude GetAttitudeTowards(IEntity other);
