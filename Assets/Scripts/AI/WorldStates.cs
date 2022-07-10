566c77ad8fda4a85dc7e417ab0f19dfc10b87d25
Douglas Newquist <doug.newquist@gmail.com>
2022-07-10 13:05:32 -0600

    Implemented world states

--- Assets/Scripts/AI/WorldStates.cs
+++ Assets/Scripts/AI/WorldStates.cs
@@ -1,31 +1,30 @@
+using System.Collections.Generic;
+
 namespace Phantom
 {
 	[System.Serializable]
 	public class WorldStates
 	{
+		private Dictionary<string, WorldState> states = new Dictionary<string, WorldState>();
+
 		public bool HasState(string key)
 		{
-			throw new System.NotImplementedException();
+			return states.ContainsKey(key);
 		}
 
 		public WorldState GetState(string key)
 		{
-			throw new System.NotImplementedException();
+			return states[key];
 		}
 
 		public bool TryGetState(string key, out WorldState state)
 		{
-			throw new System.NotImplementedException();
-		}
-
-		public void SetState(string key, int value)
-		{
-
+			return states.TryGetValue(key, out state);
 		}
 
 		public void SetState(WorldState state)
 		{
-
+			states[state.Key] = state;
 		}
 	}
 }
